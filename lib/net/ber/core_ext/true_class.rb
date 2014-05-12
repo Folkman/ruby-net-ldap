79b326ae3de59d3bb7c55f9c1b1a81d0fc54bf82
Lee Folkman <narrowwaymedia@gmail.com>
2014-05-12 18:00:54 -0400

    Wrong values for true/false

--- lib/net/ber/core_ext/true_class.rb
+++ lib/net/ber/core_ext/true_class.rb
@@ -7,6 +7,6 @@ module Net::BER::Extensions::TrueClass
   def to_ber
     # 20100319 AZ: Note that this may not be the completely correct value,
     # per some test documentation. We need to determine the truth of this.
-    "\001\001\001"
+    "\x01\x01\xFF".force_encoding('ASCII-8BIT')
   end
 end
